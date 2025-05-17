fbb0c0ad16ac4060aeaf3d95d30f0887385f598e
harshintech <harshgohil.coding@gmail.com>
2025-05-17 17:33:54 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { HeroSection } from "./components/HeroSection";
+import FeatureSection from "./components/FeatureSection";
+import Workflow from "./components/Workflow";
+import { Pricing } from "./components/Pricing";
+import { TestiMonials } from "./components/TestiMonials";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <TestiMonials />
+        <Footer />
+      </div>
+    </>
+  );
+}
